377c9b72ff3988e81df0e2c4f367dc40207c218a
hyunjong.lee <hyunjong.lee.s@gmail.com>
2014-03-03 18:28:37 +0900

    Create project
    - add package MySql.Data
    - implementing data base scheme generation framework

---
+++ Crawler/CrawlCore/CrawlHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrawlCore
+{
+    public static class CrawlHelper
+    {
+        public static string CrawlIt(string url, int timeout)
+        {
+            return "";
+        }
+    }
+}
---
+++ Crawler/CrawlCore/Database/DatabaseHelper.cs
@@ -0,0 +1,36 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace CrawlCore.Database
+{
+    public class DatabaseHelper
+    {
+        private readonly string _server;
+        private readonly string _userId;
+        private readonly string _password;
+        private readonly string _database;
+
+        private string DatabaseConnectionString
+        {
+            get
+            {
+                return new MySqlConnectionStringBuilder
+                {
+                    Server = _server,
+                    UserID = _userId,
+                    Password = _password,
+                    Database = _database,
+                }
+                .ConnectionString;
+            }
+        }
+        
+        public DatabaseHelper(string server, string userId, string password, string database)
+        {
+            _server = server;
+            _userId = userId;
+            _password = password;
+            _database = database;
+        }
+
+    }
+}
